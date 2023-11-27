20030806f8f7c0d620c9062b439f5bf00de5a164
Son Jeong Min <blisle@naver.com>
2023-11-28 00:56:31 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ from utils.dotdict import dotdict
 
 cfg = dotdict(
     TISTORY=dotdict(
-        ID='wingk98@naver.com',
-        PW='02631a',
-        BLOG_NAME='blisle',
-        SECRET_KEY='e49f9f3f18210237ce4cff41518928ad614bc7db1550646b46b5e52b8e19064ff7dd080e',
-        CLIENT_ID='e49f9f3f18210237ce4cff41518928ad',
-        REDIRECT_URI='https://blisle.tistory.com',
+        ID='',
+        PW='',
+        BLOG_NAME='',
+        SECRET_KEY='',
+        CLIENT_ID='',
+        REDIRECT_URI='',
     ),
 
     NOTION=dotdict(
-        TOKEN_V2='v02%3Auser_token_or_cookies%3AShQ_qAygVsdPPprvYTfpDJqJqa6GmvCbw1UCzCSxCBQ8KgGVRCw07PobWnQLQcu-plfZKNV89XMEw8ZpXkh7WdQPOr05-pZRN16D0BeYyG7OaM2kEPPI9MnARnMK5jNcdUPh',
-        TABLE_PAGE_URL='https://www.notion.so/5a5b95210b4949eb993f2c43c326efe4?v=a0846834dd6a4fca8d3c3b9c51910750&pvs=4',
+        TOKEN_V2='',
+        TABLE_PAGE_URL='',
         DOWNLOAD_DIR='~/.n2t',
         CODE_BLOCK_THEME='atom-one-dark',
 
@@ -32,7 +32,7 @@ cfg = dotdict(
     ),
 
     MAIL=dotdict(
-        ID='wingk98@gmail.com',
-        KEY='wege ecbq gccz gjpc',
+        ID='',
+        KEY='',
     )
 )
